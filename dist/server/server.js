f33a58ff7d25c1e31ffa6342cc0342d64f644820
Adan Huerta <Twinadanz@gmail.com>
2018-06-05 14:44:33 -0500

    Revert "testing ignore dist"
    This reverts commit e43a5a866bd0c6bd137de48810c88e4f759ea274.

--- dist/server/server.js
+++ dist/server/server.js
@@ -9,40 +9,15 @@ const server = http.createServer(app);
 //init the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 wss.on('connection', (ws) => {
-    ws.isAlive = true;
-    ws.on('pong', () => {
-        ws.isAlive = true;
-    });
     //when connection is established, add a simple event
     ws.on('message', (message) => {
         //log the received message and send it back to client
         console.log('received: %s', message);
-        const broadcastRegex = /^broadcast\:/;
-        if (broadcastRegex.test(message)) {
-            message = message.replace(broadcastRegex, '');
-            //Send back the message to the other clients
-            wss.clients
-                .forEach(client => {
-                if (client != ws) {
-                    client.send(`Hello, broadcast message -> ${message}`);
-                }
-            });
-        }
-        else {
-            ws.send(`Hello, you sent: ${message}`);
-        }
+        ws.send(`Hello, you sent -> ${message}`);
     });
     //send feedback to the incoming connection
     ws.send('Hi there, I am a WebSocket server on port 4000!');
 });
-setInterval(() => {
-    wss.clients.forEach((ws) => {
-        if (!ws.isAlive)
-            return ws.terminate();
-        ws.isAlive = false;
-        ws.ping(null, false, true);
-    });
-}, 10000);
 //start the Server
 server.listen(process.env.PORT || 4000, () => {
     console.log(`Server started on port ${server.address().port} :)`);
