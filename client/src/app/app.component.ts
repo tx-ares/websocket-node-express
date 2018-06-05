c139aaf9ce2a0a32b273683c4de9cbab97129c66
Adan Huerta <Twinadanz@gmail.com>
2018-06-05 16:19:31 -0500

    add deps and change styleUrls from css to scss

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { WebSocketSubject } from 'rxjs/Observable/dom/WebSocketSubject';
+
 export class Message {
     constructor(
         public sender: string,
@@ -10,7 +13,7 @@ export class Message {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
